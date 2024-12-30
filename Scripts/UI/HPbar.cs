561b3f7f9fc97acc442d9b24d9d4708a440ff237
ckd0dn <74403510+ckd0dn@users.noreply.github.com>
2024-12-30 15:34:06 +0900

    [Init]

---
+++ Scripts/UI/HPbar.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : MonoBehaviour
+{
+    private Slider currentHpSlider;
+    private Slider backGroundSlider;
+    private bool hiting;
+    public Slider CurrentHpSlider { get { return currentHpSlider; } private set { } }
+    public Slider BackGroundSlider { get { return backGroundSlider; } private set { } }
+
+    private void Awake()
+    {
+        var sliders = GetComponentsInChildren<Slider>();
+
+        for (int i = 0; i < sliders.Length; i++)
+        {
+            if (sliders[i].name == "HpSlider")
+            {
+                currentHpSlider = sliders[i];
+            }
+            else
+            {
+                backGroundSlider = sliders[i];
+            }
+        }
+    }
+}
+
